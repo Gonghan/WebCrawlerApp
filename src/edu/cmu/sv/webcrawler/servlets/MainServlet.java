79c8e35d0d6e87bd2955041ba762e93abd5ec157
Administrator <gonghan@ubuntu.(none)>
2014-06-18 15:40:51 -0700

    construct the basic structur

---
+++ src/edu/cmu/sv/webcrawler/servlets/MainServlet.java
@@ -0,0 +1,12 @@
+package edu.cmu.sv.webcrawler.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class MainServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5293515395438429354L;
+
+}
