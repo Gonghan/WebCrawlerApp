d22a1b589a593ea255a6ab72b4cf2f3cfe0768f8
Gonghan <wanggonghan@gmail.com>
2014-07-31 16:20:31 -0700

    connect to a new service

--- src/edu/cmu/sv/webcrawler/util/MongoConstants.java
+++ src/edu/cmu/sv/webcrawler/util/MongoConstants.java
@@ -1,11 +1,12 @@
 package edu.cmu.sv.webcrawler.util;
 
 public class MongoConstants {
-	public final static String HOST = "23.23.58.130";
-	public final static int PORT = 43917;
+	public final static String HOST = "23.20.33.31";
+	public final static int PORT = 53419;
 	public final static String DATABASE = "db";
 	public final static String COLLECTIONS = "webcrawler";
 	public final static String USERNAME = "gonghan";
 	public final static String PASSWORD = "123456";	
+	//no longer use
 	public final static String LOCALURL="mongodb://localhost/27017:db";
 }
